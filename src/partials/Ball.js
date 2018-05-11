4720fb9bff2f2e8b487a64dd79de2b31c06d70b9
Abdullah Toumeh <abdoullah.tomeh@gmail.com>
2018-05-11 09:47:44 -0700

    ball start moving

---
+++ src/partials/Ball.js
@@ -0,0 +1,40 @@
+import { SVG_NS} from '../settings';
+
+export default class Ball {
+    constructor(radius, boardWidth, boardHeight) {
+      this.radius = radius;
+      this.boardWidth = boardWidth;
+      this.boardHeight = boardHeight;
+      this.direction = 1;
+
+      this.reset();
+    }
+
+    reset() {
+      this.x = this.boardWidth / 2;
+      this.y = this.boardHeight / 2;
+      this.vy = Math.floor(Math.random() * 10 - 5);
+      this.vx = this.direction * (6 - Math.abs(this.vy));
+    }
+  
+
+// args you can pass in , think 
+  render (svg, player1, player2) {
+    // this.x = this.x
+
+    this.x += this.vx;
+    this.y += this.vy;
+
+
+
+    // draw all of our svg elements
+    let circle = document.createElementNS(SVG_NS, 'circle');
+    circle.setAttributeNS(null, 'r' , this.radius);
+    circle.setAttributeNS(null, 'cx' , this.x ); // x of the center point
+    circle.setAttributeNS(null, 'cy' , this.y ); // y of the center point
+    circle.setAttributeNS(null, 'fill', 'red');
+    
+    svg.appendChild(circle);
+
+}
+}
--- src/partials/Game.js
+++ src/partials/Game.js
@@ -1,6 +1,8 @@
+import Ball from './Ball';
 import Paddle from './Paddle';
 import Board from './Board';
 import { SVG_NS, KEYS } from '../settings';
+
 export default class Game {
 
 	constructor(element, width, height) {
@@ -11,6 +13,7 @@ export default class Game {
 		this.gameElement = document.getElementById(this.element);
 
 		this.board = new Board(this.width, this.height);
+		this.ball = new Ball(8, this.width,this.height);
 
 		this.paddleWidth = 8;
 		this.paddleHeight = 56;
@@ -62,7 +65,7 @@ export default class Game {
 		this.player1.render(svg);
 		this.player2.render(svg);
 
-
+		this.ball.render(svg, this.player1, this.player2);
 
 	}
 
