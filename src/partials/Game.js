699d18dafb7d8119663e7b559e3ed746f745d856
Abdullah Toumeh <abdoullah.tomeh@gmail.com>
2018-05-12 14:17:33 -0700

    adding another ball and adding left right movement

--- src/partials/Game.js
+++ src/partials/Game.js
@@ -20,48 +20,51 @@ export default class Game {
 			}
 		});
 
-
 		this.board = new Board(this.width, this.height);
 		this.ball1 = new Ball(8, this.width,this.height);
 		this.ball2 = new Ball(8, this.width,this.height);
-
-
 		this.paddleWidth = 8;
 		this.paddleHeight = 56;
-		this.boardGap = 10;
-		
+		this.boardGap = 10;	
+
 		this.player1 = new Paddle(
 		this.height,
+		this.width,
 		this.paddleWidth,
 		this.paddleHeight,
 		this.boardGap,
 		((this.height - this.paddleHeight) / 2),
+		KEYS.w,
+		KEYS.s,
 		KEYS.a,
-		KEYS.z
+		KEYS.d,
+		'player1'
 		);
 		
 		this.player2 = new Paddle(
 		this.height,
+		this.width,
 		this.paddleWidth,
 		this.paddleHeight,
 		(this.width - this.boardGap - this.paddleWidth),
 		((this.height - this.paddleHeight) / 2),
 		KEYS.up,
-		KEYS.down
+		KEYS.down,
+		KEYS.left,
+		KEYS.right,
+		'player2'
 		);
 
 		this.score1 = new Score(this.width / 2 - 50, 30, 30);
 		this.score2 = new Score(this.width / 2 + 25, 30, 30);
 
-		// Other code goes here...
 	}// constructor
+
 	render() {
 		//return pause
 		if (this.pause) {
 			return;
 		}
-
-		// More code goes here...
 
 		//be sure to empty out the last frame before re-rendering
 		this.gameElement.innerHTML = '';
@@ -70,18 +73,12 @@ export default class Game {
 		svg.setAttributeNS(null, 'width', this.width);
 		svg.setAttributeNS(null, 'height', this.height);
 		svg.setAttributeNS(null, 'viewBox', `0 0 ${this.width} ${this.height}`);
-
 		this.gameElement.appendChild(svg);
-
 		this.board.render(svg);
 		this.player1.render(svg);
 		this.player2.render(svg);
-
 		this.ball1.render(svg, this.player1, this.player2);
 		this.ball2.render(svg, this.player1, this.player2);
-
-
-
 		// render and update the score component based on player score
 		this.score1.render(svg, this.player1.score);
 		this.score2.render(svg, this.player2.score);
--- src/partials/Paddle.js
+++ src/partials/Paddle.js
@@ -2,8 +2,9 @@ import { SVG_NS} from '../settings';
 
 
 export default class Paddle {
-    constructor(boardHeight, width, height, x, y, up, down) {
+    constructor(boardHeight, boardWidth, width, height, x, y, up, down, leftplayer1, rightplayer1, player) {
       this.boardHeight = boardHeight;
+      this.boardWidth = boardWidth;
       this.width = width;
       this.height = height;
       this.x = x;
@@ -11,30 +12,53 @@ export default class Paddle {
       this.speed = 10;
       this.score = 0;
 
-        document.addEventListener('keydown', event => {
-            switch (event.key) {
-              case up:
-                this.up();
-                break;
-              case down:
-                this.down();
-                break;
-            }
-          });
+      this.player = player;
+      this.keyState = {};
+
+      document.addEventListener('keydown', event => {
+        this.keyState[event.key || event.which] = true;
+      }, true);
+  
+      document.addEventListener('keyup', event => {
+        this.keyState[event.key || event.which] = false;
+      }, true);
+
+      document.addEventListener('keyleft', event => {
+        this.keyState[event.key || event.which] = true;
+      }, true);
+  
+      document.addEventListener('keyright', event => {
+        this.keyState[event.key || event.which] = false;
+      }, true);
+
+
               
     }// constructor
 
     up() {
-        // get the max number...
-
-        // this.y = this.y - this.speed;
         this.y = Math.max(0, this.y - this.speed);
     }
 
     down() {
         this.y = Math.min(this.boardHeight - this.height, this.y + this.speed );
-
     }
+
+    leftplayer1() {
+        this.x = Math.max(10, this.x - this.speed);
+    }
+
+    rightplayer1() {
+        this.x = Math.min((this.boardWidth) / 5, this.x + this.speed);
+    }
+
+    leftplayer2() {
+        this.x = Math.max((this.boardWidth) - (this.boardWidth) / 5, this.x - this.speed);
+    }
+
+    rightplayer2() {
+        this.x = Math.min(this.boardWidth - this.width - 10 , this.x + this.speed);
+    }
+    
     // helper method
     coordinates(x, y, width, height) {
         let leftX = x;
@@ -48,6 +72,32 @@ export default class Paddle {
     
     //...
     render (svg) {
+        // Player movement
+        if (this.keyState['w'] && this.player === 'player1') {
+            this.up();
+        }
+        if (this.keyState['s'] && this.player === 'player1') {
+            this.down();
+        }
+        if (this.keyState['a'] && this.player === 'player1') {
+            this.leftplayer1();
+        }
+        if (this.keyState['d'] && this.player === 'player1') {
+            this.rightplayer1();
+        }
+//////////////////////
+        if (this.keyState['ArrowUp'] && this.player === 'player2') {
+            this.up();
+        }
+        if (this.keyState['ArrowDown'] && this.player === 'player2') {
+            this.down();
+        }
+        if (this.keyState['ArrowLeft'] && this.player === 'player2') {
+            this.leftplayer2();
+        }
+        if (this.keyState['ArrowRight'] && this.player === 'player2') {
+            this.rightplayer2();
+        }
         // draw all of our svg elements
         let rect = document.createElementNS(SVG_NS, 'rect');
 		rect.setAttributeNS(null, 'fill' , 'white');
@@ -59,4 +109,4 @@ export default class Paddle {
         svg.appendChild(rect);
 
     }
-  }
+}
